49505842f40aeaff6bb569a66ece2dd0cc81b44d
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-03-07 14:04:14 +0900

    firstcommit

---
+++ src/test/java/me/study/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
